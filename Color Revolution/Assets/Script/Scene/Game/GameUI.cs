e957cb6de451349046bcda36ade296963d9240e9
scott <autumn192837465@yahoo.com.tw>
2023-01-22 18:27:06 +0900

    feat : add assets

---
+++ Color Revolution/Assets/Script/Scene/Game/GameUI.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    public Button showPlaceableButton;
+    
+    public enum ButtonType
+    {
+        
+    }
+
+    [Serializable]
+    public class ButtonInfo
+    {
+        public ButtonType Type;
+        public Button Button;
+    }
+
+    [SerializeField] private List<ButtonInfo> buttonList;
+    public Action<ButtonType> OnClickButton;    
+        
+
+    private void Awake()
+    {
+        foreach(ButtonInfo buttonInfo in buttonList)
+        {
+            buttonInfo.Button.onClick.AddListener(() => OnClickButton?.Invoke(buttonInfo.Type));
+        }
+        
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    public void InitializeUI()
+    {
+
+    }
+}
